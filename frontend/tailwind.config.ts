064868684636aeceefab4b1fd65c48e602894605
Norton Choy <nomvus848@gmail.com>
2025-08-21 00:33:39 -0700

    Added tailwind config

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+        serif: ['var(--font-serif)'],
+        inter: ['var(--font-inter)'],
+      },
+    },
+  },
+}
