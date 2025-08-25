d96024163fecbf17ab1aeb12311a444118b232c8
Norton Choy <nomvus848@gmail.com>
2025-08-25 05:42:29 -0700

    Added create_user function

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,7 +3,8 @@ import uuid
 from sqlmodel import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models.user import User
+from app.models.user import User, UserCreate
+from app.services import auth_service
 
 async def get_user_by_id(db: AsyncSession, user_id: uuid.UUID) -> User | None:
   """
@@ -32,3 +33,26 @@ async def get_user_by_email(db: AsyncSession, email: str) -> User | None:
   statement = select(User).where(User.email == email)
   result = await db.exec(statement)
   return result.first()
+
+async def create_user(db: AsyncSession, user_in: UserCreate) -> User:
+  """
+  Create a new user in the database.
+
+  Args:
+      `db`: Async database session for executing queries.
+      `user_in`: `UserCreate` object containing the details of the user to be created.
+
+  Returns:
+      The newly created User object.
+  """
+  new_user = User(
+    email = user_in.email,
+    username = user_in.username,
+    password = auth_service.hash_password(user_in.password) if user_in.password else None,
+    google_id = user_in.google_id
+  )
+
+  db.add(new_user)
+  await db.commit()
+  await db.refresh(new_user)
+  return new_user
