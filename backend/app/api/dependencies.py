889863cc9b6ad02ae024ac5b06366acceca37fa0
Norton Choy <nomvus848@gmail.com>
2025-08-25 05:43:34 -0700

    Added auth dependencies

---
+++ backend/app/api/dependencies.py
@@ -0,0 +1,65 @@
+from typing import Generator
+import uuid
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_db
+from app.models.user import User
+from app.services import auth_service, user_service
+
+# OAuth2PasswordBearer is used to extract the token from the Authorization header
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_current_user(db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)) -> User:
+  """
+  FastAPI dependency to authenticate and retrieve the current user based on the provided JWT token.
+
+  Args:
+      `db`: Async database session for executing queries.
+      `token`: The OAuth2 (JWT) token extracted from the Authorization header.
+
+  Returns:
+      The authenticated User object.
+
+  Raises:
+      HTTPException: If the token is invalid, the user is not found, or the user is inactive.
+  """
+  credentials_exception = HTTPException(
+    statuc_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+  )
+
+  token_data = auth_service.get_data_from_token(token)
+  if token_data is None:
+    raise credentials_exception
+  
+  user = await user_service.get_user_by_id(db, token_data.user_id)
+  if user is None:
+    raise credentials_exception
+
+  return user
+
+# Wrapper dependency for readability
+async def get_current_active_user(current_user: User = Depends(get_current_user)) -> User:
+  """
+  FastAPI dependency to ensure the current user is active.
+
+  Args:
+      `current_user`: The current authenticated user, retrieved using the `get_current_user` dependency.
+
+  Returns:
+      The current active User object.
+
+  Raises:
+      HTTPException: If the user is not active.
+  """
+  if not current_user.is_active:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Inactive user"
+    )
+  
+  return current_user
