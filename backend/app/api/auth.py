8c593da873bb2d9bb538a628f1bda732d3c2fb0c
Norton Choy <nomvus848@gmail.com>
2025-08-28 04:20:06 -0700

    Pass auth tests

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -40,7 +40,7 @@ async def register(
     existing_user_email = await auth_service.get_user_by_email(session, user_create.email)
     if existing_user_email:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registerd"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
         )
 
     user = await user_service.create_user(session, user_create)
