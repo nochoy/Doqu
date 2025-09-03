5566b5a5e35715cda611754260edfbc2902031d5
Norton Choy <nomvus848@gmail.com>
2025-09-02 19:56:54 -0700

    Updated deprecated functions + added jsdoc comments

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -2,19 +2,30 @@ import * as z from "zod";
 
 // Zod schema for login/signup form
 export const AuthFormSchema = z.object({
-  email: z.string().email({ message: "Invalid email address." }),
-  username: z.string().min(1, { message: "Username cannot be blank." }),
-  password: z.string().min(1, { message: "Password cannot be blank." })
+  email: z.email({ error: "Invalid email address." }),
+  username: z.string().min(1, { error: "Username cannot be blank." }),
+  password: z.string().min(1, { error: "Password cannot be blank." })
 });
 
 // Extract the inferred type
 export type AuthFormInput = z.infer<typeof AuthFormSchema>;
 
+/**
+ * Represents an authentication token.
+ *
+ * @property {string} access_token - The token string used for accessing protected resources.
+ * @property {string} token_type - The type of the token, typically "Bearer".
+ */
 export interface Token {
   access_token: string,
   token_type: string,
 }
 
+/**
+ * Represents the input required for Google login.
+ *
+ * @property {string} google_id_token - The ID token provided by Google after user authentication.
+ */
 export interface GoogleLoginInput {
   google_id_token: string;
 }
