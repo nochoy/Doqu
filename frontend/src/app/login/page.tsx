72bc489d3e4cc3304fc34be5389ed9a0bce4b7d2
Norton Choy <nomvus848@gmail.com>
2025-09-04 00:02:57 -0700

    Changed bg color to accent

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/auth/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-accent p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         {/* Logo */}
         {/* <a href="#" className="flex items-center gap-2 self-center font-medium">
