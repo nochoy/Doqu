381fbdcdfeebf72a1682848e267f21a3fdd88ce1
Norton Choy <nomvus848@gmail.com>
2025-09-07 01:19:12 -0700

    Added pydantic models for google login endpoint

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -93,11 +93,21 @@ class UserLogin(BaseModel):
 
 class GoogleLogin(BaseModel):
     """
-    Pydantic model for Google user login with `google_id_token`.
+    Pydantic model for Google user login with `code`.
     """
 
-    google_id_token: str
+    code: str
 
+class GoogleUserData(BaseModel):
+    """
+    Pydantic model for storing Google user data.
+
+    This model includes fields for the user's Google ID, email, and name.
+    """
+
+    google_id: str
+    email: str
+    name: str
 
 class Token(BaseModel):
     """
