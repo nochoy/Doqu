02003b29abca50984f21a4b4c6baf40b7164342c
Norton Choy <nomvus848@gmail.com>
2025-09-20 15:41:08 -0700

    Revert "Updated FE .env file path for nextjs config"
    This reverts commit 3cf3de3f26066d3eb266002269ca48c5b2e58b16.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from 'next';
-import path from 'path';
-import dotenv from 'dotenv';
-
-// Custom .env file path
-dotenv.config({ path: path.resolve(__dirname, '../.envs/.local/.frontend') });
 
 const nextConfig: NextConfig = {
   /* config options here */
