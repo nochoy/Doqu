435113d50101ee73af52dbaeb61030e11e21799c
Norton Choy <nomvus848@gmail.com>
2025-09-29 23:38:39 -0700

    Added loading state + fixed QuizListModal dynamic padding

--- frontend/src/components/layout/navbar/QuizListModal.tsx
+++ frontend/src/components/layout/navbar/QuizListModal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useState, Fragment } from "react";
 
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
@@ -14,44 +14,13 @@ import { QuizReadData } from "@/types/quiz";
 
 interface QuizListProps {
   className?: string;
+  quizzes: QuizReadData[];
   selectedQuiz?: string;
   onQuizSelect: (quizId: string) => void;
+  isLoading: boolean;
 }
 
-function QuizList({ className, selectedQuiz, onQuizSelect }: QuizListProps) {
-  const { currentUser, isAuthenticated } = useAuth();
-  const [userQuizzes, setUserQuizzes] = useState<QuizReadData[]>([]);
-
-  useEffect(() => {
-    const fetchUserQuizzes = async () => {
-      if(!isAuthenticated || !currentUser?.id) {
-        setUserQuizzes([]);
-        return;
-      }
-      try {
-
-        const params = new URLSearchParams({
-          owner_id: currentUser.id,
-          offset: '0',
-          limit: '100',
-        });
-
-        const response = await authenticatedFetch(`${process.env.NEXT_PUBLIC_API_URL}/api/quizzes/?${params.toString()}`);
-
-        const result = await response.json();
-
-        if(!response.ok) {
-          throw new Error(result.detail || ' An error occured retrieving quizzes');
-        }
-        setUserQuizzes(result);
-      } catch (err) {
-        console.error('Error retrieving user quizzes: ', err);
-        setUserQuizzes([]);
-      }
-    }
-
-    fetchUserQuizzes();
-  }, [currentUser?.id, isAuthenticated]);
+function QuizList({ className, quizzes, selectedQuiz, onQuizSelect, isLoading }: QuizListProps) {
 
   const handleQuizClick = (quizId: string) => {
     if(selectedQuiz === quizId) {
@@ -61,32 +30,34 @@ function QuizList({ className, selectedQuiz, onQuizSelect }: QuizListProps) {
     }
   }
 
+  // quizzes = Array.from({ length: 100 }, (_, i) => ({ id: `${i}`, title: `Quiz${i}`}))
+
+  if (isLoading) {
+    return <div className="p-4 text-center text-muted-foreground">Loading quizzes...</div>
+  }
+
+  if (quizzes.length === 0) {
+    return <div className="p-4 text-center text-muted-foreground">No quizzes found. Create your first quiz!</div>
+  }
+
   return (
     <>
-      { isAuthenticated ? (
-        <ScrollArea className={cn('flex-1 overflow-auto', className)}>
-          {userQuizzes.map((quiz) => (
-            <>
-              <div 
-                key={quiz.id}
-                onClick={() => handleQuizClick(quiz.id)}
-                role='button'
-                tabIndex={0}
-                aria-pressed={selectedQuiz === quiz.id}
-                className={`text-sm hover:bg-accent p-2 ${selectedQuiz === quiz.id ? 'bg-accent border-l-4 border-l-primary pl-1' : ''}`}
-              >
-                {quiz.title}
-              </div>
-              <Separator className=""/>
-            </>
-          ))}
-        </ScrollArea>
-        ) : (
-          <p>
-            Sign in
-          </p>
-        )
-      }
+      <ScrollArea className={cn('flex-1 overflow-auto', className)}>
+        {quizzes.map((quiz) => (
+          <Fragment key={quiz.id}>
+            <div 
+              onClick={() => handleQuizClick(quiz.id)}
+              role='button'
+              tabIndex={0}
+              aria-pressed={selectedQuiz === quiz.id}
+              className={`text-sm hover:bg-accent p-2 ${selectedQuiz === quiz.id ? 'bg-accent border-l-4 border-l-primary pl-1' : ''}`}
+            >
+              {quiz.title}
+            </div>
+            <Separator className=""/>
+          </Fragment >
+        ))}
+      </ScrollArea>
     </>
   )
 }
@@ -96,6 +67,50 @@ export default function QuizListModal() {
   const [openMobile, setOpenMobile] = useState<boolean>(false);
   const [openDesktop, setOpenDesktop] = useState<boolean>(false);
   const [selectedQuiz, setSelectedQuiz] = useState<string>('');
+  const [userQuizzes, setUserQuizzes] = useState<QuizReadData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  
+  const { currentUser, isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    const fetchUserQuizzes = async () => {
+      if(!isAuthenticated || !currentUser?.id) {
+        setUserQuizzes([]);
+        return;
+      }
+
+      try {
+        console.log('fetching quizzes');
+        setIsLoading(true);
+
+        const params = new URLSearchParams({
+          owner_id: currentUser.id,
+          offset: '0',
+          limit: '100',
+        });
+
+        const response = await authenticatedFetch(`${process.env.NEXT_PUBLIC_API_URL}/api/quizzes/?${params.toString()}`);
+        const result = await response.json();
+
+        if(!response.ok) {
+          throw new Error(result.detail || "An error occurred fetching user's quizzes");
+        }
+        setUserQuizzes(result);
+      } catch (err) {
+        console.error("Error fetching user's quizzes: ", err);
+        setUserQuizzes([]);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (openMobile || openDesktop) {
+      fetchUserQuizzes();
+    } else {
+      setUserQuizzes([]);
+      setSelectedQuiz('');
+    }
+  }, [currentUser?.id, isAuthenticated, openMobile, openDesktop]);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -108,12 +123,12 @@ export default function QuizListModal() {
 
   const handleMobileOpenChange = (isOpen: boolean) => {
     setOpenMobile(isOpen);
-    setSelectedQuiz('');
+    if (!isOpen) setSelectedQuiz('');
   }
 
   const handleDesktopOpenChange = (isOpen: boolean) => {
     setOpenDesktop(isOpen);
-    setSelectedQuiz('');
+    if (!isOpen) setSelectedQuiz('');
   }
 
   return (
@@ -130,15 +145,17 @@ export default function QuizListModal() {
           <DrawerContent className="max-h-3/4">
             <DrawerHeader>
               <DrawerTitle>Select a quiz</DrawerTitle>
-              <DrawerDescription>Description</DrawerDescription>
+              <DrawerDescription>Choose a quiz to host a game</DrawerDescription>
             </DrawerHeader>
 
             {/* Quiz List */}
             <form onSubmit={handleSubmit} className="flex flex-col flex-1 min-h-0">
               <QuizList 
-                className='px-4' 
+                className='px-4'
+                quizzes={userQuizzes}
                 selectedQuiz={selectedQuiz}
                 onQuizSelect={setSelectedQuiz}
+                isLoading={isLoading}
               />
 
               {/* Buttons */}
@@ -166,30 +183,31 @@ export default function QuizListModal() {
             </Button>
           </DialogTrigger>
 
-          <DialogContent className="h-1/2">
+          <DialogContent className="flex flex-col max-h-[50vh]">
             <DialogHeader>
               <DialogTitle>Select a quiz</DialogTitle>
-              <DialogDescription>Description</DialogDescription>
+              <DialogDescription>Choose a quiz from your collection to host a game</DialogDescription>
             </DialogHeader>
 
             {/* Quiz List */}
-            <form onSubmit={handleSubmit} className="flex flex-col flex-1 min-h-0">
-              <QuizList 
+            <form onSubmit={handleSubmit} id="current-user-quizzes" className="flex min-h-0">
+              <QuizList
                 selectedQuiz={selectedQuiz}
                 onQuizSelect={setSelectedQuiz}
+                quizzes={userQuizzes}
+                isLoading={isLoading}
               />
-
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant='secondary'>
-                    Cancel
-                  </Button>
-                </DialogClose>
-                <Button type='submit' disabled={!selectedQuiz}>
-                  Select
-                </Button>
-              </DialogFooter>
             </form>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant='secondary'>
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type='submit' form='current-user-quizzes' disabled={!selectedQuiz}>
+                Select
+              </Button>
+            </DialogFooter>
           </DialogContent>
         </Dialog>
       </div>
