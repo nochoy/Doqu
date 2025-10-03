43ef575574e5c0506a86ff73059a1bedf53ff7b4
Norton Choy <nomvus848@gmail.com>
2025-10-02 21:48:58 -0700

    Moved className params to Image instead of Link

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,15 +1,16 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo({ className = '' }) {
   return (
-    <Link href='/' className={className}>
+    <Link href='/' className='flex items-center'>
       <Image
         src='/DoquLogo1.svg'
         alt='Doqu logo'
         width={60}
         height={120}
-        className='h-8 sm:h-10 w-auto'
+        className={cn('h-10 w-auto', className)}
       />
     </Link>
   )
