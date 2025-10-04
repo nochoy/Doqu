32ee7d8d4e7d33a87e6f969aa4ffa2550b3d06d4
Norton Choy <nomvus848@gmail.com>
2025-10-03 22:31:04 -0700

    Improved mobile responsiveness for account dropdown menu and navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -3,10 +3,9 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
-import { CaretUpIcon, ListIcon, PlusSquareIcon, UserCircleIcon } from '@phosphor-icons/react';
+import { ListIcon, PlusSquareIcon } from '@phosphor-icons/react';
 
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTrigger } from "@/components/ui/sheet";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
 import Logo from '@/components/shared/Logo';
 import AccountDropdownMenu from '@/components/layout/navbar/AccountDropdownMenu';
@@ -58,7 +57,7 @@ export default function Navbar() {
     const CreateQuizButton = (
       <>
         <Button variant='default' onClick={() => setCreateQuizFormOpen(true)} className='mx-0 sm:mx-2'>
-          <PlusSquareIcon/>
+          <PlusSquareIcon size={18}/>
           Create
         </Button>
         {isCreateQuizFormOpen && 
@@ -66,67 +65,38 @@ export default function Navbar() {
         }
       </>
     )
-
-    const MobileNavbar = (
-      <nav className="flex items-center mx-auto relative h-10">
+  
+  return (
+    <header className="bg-sidebar text-sidebar-foreground border-sidebar-border p-2 border-b w-full">
+      { isMobile ? ( // Mobile Navbar
+        <nav className="flex items-center mx-auto relative h-10">
         <Sheet open={isMobileSidebarOpen} onOpenChange={setMobileSidebarOpen}>
           <SheetTrigger>
               <ListIcon size={24}/>
               <span className='sr-only'>Toggle navigation menu</span>
           </SheetTrigger>
 
-          <SheetContent side="left" className=''>
+          {/* Side drawer */}
+          <SheetContent side="left" className='flex flex-col'>
             <SheetHeader className="flex items-center">
               <Logo/>
             </SheetHeader>
 
             <div className='flex-1 flex flex-col px-3'>
-              <ThemeToggle/>
               {HostQuizButton}
               {JoinRoomButton}
               {CreateQuizButton}
-              <AccountDropdownMenu/>
+              <ThemeToggle/>
             </div>
 
             <SheetFooter className='mt-auto'>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant='ghost'>
-                    <UserCircleIcon size={32}/>
-                    Username
-                    email
-                    <CaretUpIcon className="ml-auto" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  side="top"
-                  align="end"
-                  className="w-[--radix-popper-anchor-width]"
-                >
-                  <DropdownMenuItem>
-                    <span>Account</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Billing</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Sign out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <AccountDropdownMenu/>
             </SheetFooter>
           </SheetContent>
         </Sheet>
         <Logo className='absolute left-1/2 transform -translate-x-1/2'/>
       </nav>
-    )
-  
-  return (
-    <header className="bg-sidebar text-sidebar-foreground border-sidebar-border p-2 border-b w-full">
-      { isMobile ? (
-        MobileNavbar
-      ) : (
-        // Desktop navbar
+      ) : ( // Desktop navbar
         <nav className="flex justify-between items-center mx-auto">
           <Logo/>
           <div className="flex items-center gap-1">
--- frontend/src/components/layout/navbar/AccountDropdownMenu.tsx
+++ frontend/src/components/layout/navbar/AccountDropdownMenu.tsx
@@ -2,15 +2,18 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { CaretDownIcon, GearIcon, SignInIcon, SignOutIcon, UserCircleIcon, UserIcon, UserPlusIcon } from '@phosphor-icons/react';
+import { useRouter } from 'next/navigation';
+import { CaretDownIcon, CaretUpDownIcon, GearIcon, SignInIcon, SignOutIcon, UserCircleIcon, UserIcon, UserPlusIcon } from '@phosphor-icons/react';
 
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
 import { useAuth } from '@/hooks/useAuth';
-import { useRouter } from 'next/navigation';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 
 export default function AccountDropdownMenu() {
   const { currentUser, isAuthenticated, logout } = useAuth();
   const router = useRouter()
+  const isMobile = useMediaQuery();
 
   const handleLogout = () => {
     logout();
@@ -19,67 +22,92 @@ export default function AccountDropdownMenu() {
 
   return (
     <DropdownMenu>
-    {/* Profile Dropdown Button*/}
-    <DropdownMenuTrigger className='flex items-center cursor-pointer p-1 h-9 w-auto gap-0.5'>
-        {isAuthenticated ? <UserCircleIcon size={28} weight='fill'/> : <UserCircleIcon size={28}/>}
-        <CaretDownIcon weight='bold'/>
-    </DropdownMenuTrigger>
-
-    {/* Popup */}
-    <DropdownMenuContent align='end' className='w-[12rem]'>
-      {/* Username & Email */}
-      <DropdownMenuLabel>
-        { isAuthenticated ?
-          <p className='truncate'>
-            { currentUser?.username }
-          </p> :
-          <p className='justify-self-center'>
-            Not signed in
-          </p>
-        }
-        <p className='text-muted-foreground text-sm font-normal truncate'>
-          { currentUser?.email }
-        </p>
-      </DropdownMenuLabel>
-      <DropdownMenuGroup>
-        { isAuthenticated &&
-          <>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem asChild>
-              <Link href={`/profile/${currentUser?.id}`}>
-                <UserIcon/>
-                View Profile
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href='/settings'>
-                  <GearIcon/>
-                  Settings
-              </Link>
-            </DropdownMenuItem>
-          </>
-        }
-        {/* Login/Logout button */}
+      { isMobile ? (
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='py-6 px-2 rounded-xl'>
+            {isAuthenticated ? <UserCircleIcon size={32} weight='fill'/> : <UserCircleIcon size={32} className='text-muted-foreground'/>}
+            <div className='flex flex-col items-start'>
+              { isAuthenticated ? (
+                <>
+                  <span className='truncate'>{currentUser?.username}</span>
+                  <span className='text-muted-foreground text-xs font-normal truncate'>{currentUser?.email}</span>
+                </>
+              ) : (
+                <p className='justify-self-center text-muted-foreground font-normal'>Not signed in</p>
+              )
+            }
+            </div>
+            <CaretUpDownIcon size={24} weight='light' className="ml-auto"/>
+          </Button>
+        </DropdownMenuTrigger>
+      ) : (
+        <DropdownMenuTrigger className='flex items-center cursor-pointer p-1 h-9 w-auto gap-0.5'>
+            {isAuthenticated ? <UserCircleIcon size={28} weight='fill'/> : <UserCircleIcon size={28}/>}
+            <CaretDownIcon weight='bold'/>
+        </DropdownMenuTrigger>
+      )}
+      {/* Popup */}
+      <DropdownMenuContent 
+        align='end' 
+        side={isMobile ? 'top' : 'bottom'} 
+        className={isMobile ? 'w-[--radix-popper-anchor-width]' : 'w-[12rem]'}
+      >
+        {/* Username & Email */}
+        <DropdownMenuLabel>
           { isAuthenticated ?
-            <DropdownMenuItem onClick={handleLogout} className='flex items-center gap-2'>
-                <SignOutIcon size={16}/>Logout 
-            </DropdownMenuItem> : 
+            <p className='truncate'>{ currentUser?.username }</p>
+            :
+            <p className='justify-self-center'>Not signed in</p>
+          }
+          <p className='text-muted-foreground text-sm font-normal truncate'>
+            { currentUser?.email }
+          </p>
+        </DropdownMenuLabel>
+
+        {/* Menu Options */}
+        <DropdownMenuGroup>
+          {/* Profile + Settings Links */}
+          { isAuthenticated &&
+            <>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem asChild>
+                <Link href={`/profile/${currentUser?.id}`}>
+                  <UserIcon/>
+                  View Profile
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link href='/settings'>
+                    <GearIcon/>
+                    Settings
+                </Link>
+              </DropdownMenuItem>
+            </>
+          }
+          {/* Login/Logout button */}
+            { isAuthenticated ?
+              (
+                <DropdownMenuItem onClick={handleLogout} className='flex items-center gap-2'>
+                    <SignOutIcon size={16}/>Logout 
+                </DropdownMenuItem> 
+              ) : (
+                <DropdownMenuItem asChild>
+                  <Link href='/login'>
+                      <SignInIcon size={16}/>Login 
+                  </Link>
+                </DropdownMenuItem>
+              )
+            }
+          {/* Register button */}
+          { !isAuthenticated && 
             <DropdownMenuItem asChild>
-              <Link href='/login'>
-                  <SignInIcon size={16}/>Login 
+              <Link href='/register'>
+                <UserPlusIcon size={16}/> Register
               </Link>
             </DropdownMenuItem>
           }
-        {/* Register button */}
-        { !isAuthenticated && 
-          <DropdownMenuItem asChild>
-            <Link href='/register'>
-              <UserPlusIcon size={16}/> Register
-            </Link>
-          </DropdownMenuItem>
-        }
-      </DropdownMenuGroup>
-    </DropdownMenuContent>
-  </DropdownMenu>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
